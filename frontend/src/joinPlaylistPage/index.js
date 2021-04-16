5f9f7ea14b92e43aae82c1f233f8c8cf95023efe
jshi213 <jshi213@aucklanduni.ac.nz>
2021-04-16 12:52:43 +1200

    added textbox for playlist joining code

--- frontend/src/joinPlaylistPage/index.js
+++ frontend/src/joinPlaylistPage/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import styles from "./style.module.css";
 import { useHistory } from 'react-router-dom';
-import { Button, Container, makeStyles } from '@material-ui/core';
+import { Button, TextField, makeStyles } from '@material-ui/core';
 import { green, purple } from '@material-ui/core/colors';
 
 const useStyles = makeStyles({
     primaryButton: {
         background: "#30A0F5",
         borderRadius: 30,
-        padding: "20px 40px",
+        padding: "18px 40px",
         width: "30%",
         fontSize: 18,
+    },
+    textField: {
+        background: "#595959",
+        width: "300px",
+        fontSize: 18,
+        color: "white",
     },
 });
 
@@ -31,8 +37,29 @@ export default function JoinPlaylistPage() {
                 >
                     Back
                 </Button>
-            <div className={styles.buttonContainer}>
-
+            <div className={styles.textFieldContainer}>
+                <form noValidate autoComplete="off">
+                    <TextField 
+                        classes={{ root: classes.textField }}
+                        id="standard-basic" 
+                        variant="outlined" 
+                        size="large" 
+                        label="Playlist code" 
+                        InputProps={{
+                            style: { color: '#fff' },
+                        }}
+                        InputLabelProps={{
+                            style: { color: '#fff' },
+                        }}
+                    />
+                </form>
+                <Button
+                    classes={{ root: classes.primaryButton }}
+                    onClick={handleBack}
+                    style={{ position: 'absolute', bottom: 0, width: 300 }}
+                >
+                    Enter
+                </Button>
             </div>
         </div>
     );
