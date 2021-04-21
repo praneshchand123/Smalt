b5f9f062efc97318cc974f9c9744e71804841647
闵静纬 <minjingwei@minjingweideMacBook-Pro.local>
2021-04-21 16:29:07 +0800

    added homePage

---
+++ frontend/src/homePage/index.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import styles from "./style.module.css";
+import { Button, TextField, makeStyles, Avatar, AvatarGroup } from '@material-ui/core';
+
+
+const useStyles = makeStyles({
+    primaryButton: {
+        background: "#30A0F5",
+        borderRadius: 30,
+        padding: "18px 40px",
+        width: "30%",
+        fontSize: 18,
+    },
+    textField: {
+        background: "#595959",
+        width: "300px",
+        fontSize: 18,
+        color: "white",
+    },
+    paragraphy: {
+        background: "#333333",
+        padding: "5px",
+        fontSize: 15,
+    },
+
+    headLine: {
+        background: "#333333",
+        padding: "5px",
+        fontSize: 20,
+    }
+
+
+});
+
+export default function JoinPlaylistPage() {
+    const classes = useStyles();
+
+    const handleLoginClick = () => {
+        console.log('button click');
+    }
+
+    const handleFetch = () => {
+        console.log('Fetch Device')
+    }
+
+    const handleAvatar = () => {
+        <Avatar />
+    }
+
+    
+
+    return (
+        <div className={styles.rootContainer}>
+            <div>
+                <Button className={{root: classes.primaryButton}}
+                onClick={handleLoginClick}>Login</Button>
+            </div>
+            <div className={styles.rootContainer}>
+                <p className={{root: classes.paragraphy}}>Playlist is empty now, please add songs</p>
+                <TextField 
+                        classes={{ root: classes.textField }}
+                        id="standard-basic" 
+                        variant="outlined" 
+                        size="large" 
+                        label="Search songs code" 
+                        InputProps={{
+                            style: { color: '#fff' },
+                        }}
+                        InputLabelProps={{
+                            style: { color: '#fff' },
+                        }}
+                    />
+            </div>
+            <div>
+                <p className={{root: classes.headLine}}>Device</p>
+                <Button className={{root: classes.primaryButton}} 
+                onClick={handleFetch}></Button>
+            </div>
+
+            <div>
+                <p lassName={{root: classes.headLine}}>Active Friends</p>
+                <AvatarGroup max={4}>
+                    {handleAvatar}
+                </AvatarGroup>
+            </div>
+            
+        </div>
+    );
+}
