fa9a6c0ae4aa3f6ae7b7ce1dbb4b02f71fb91a24
闵静纬 <minjingwei@minjingweideMacBook-Pro.local>
2021-04-23 19:33:41 +0800

    Update homepage

--- frontend/src/homePage/index.js
+++ frontend/src/homePage/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import styles from "./style.module.css";
 import { Button, TextField, makeStyles } from '@material-ui/core';
+import { BottomNavigation } from '@material-ui/core';
 
 
 const useStyles = makeStyles({
-    primaryButton: {
+    loginButton: {
         background: "#30A0F5",
         borderRadius: 30,
-        padding: "18px 40px",
+        padding: "15px 30px",
         width: "30%",
-        fontSize: 18,
+        fontSize: 15,
+    },
+    deviceButton: {
+        background: "#30A0F5",
+        borderRadius: 30,
+        padding: "15px 30px",
+        width: "100%",
+        fontSize: 15,
     },
     textField: {
         background: "#595959",
@@ -17,65 +25,48 @@ const useStyles = makeStyles({
         fontSize: 18,
         color: "white",
     },
-    paragraphy: {
-        background: "#333333",
-        padding: "5px",
-        fontSize: 15,
-    },
-
-    headLine: {
-        background: "#333333",
-        padding: "5px",
-        fontSize: 20,
-    }
-
-
 });
 
 export default function HomePage() {
-    console.log("homepage");
+
     const classes = useStyles();
 
     const handleLoginClick = () => {
         console.log('button click');
-    }
+    };
 
     const handleFetch = () => {
-        console.log('Fetch Device')
-    }
+        console.log('Fetch Device');
+    };
 
     return (
         <div className={styles.rootContainer}>
             <div>
-                <Button className={{ root: classes.primaryButton }}
+                <Button classes={{ root: classes.loginButton }}
                     onClick={handleLoginClick}>Login
                 </Button>
             </div>
-            <div className={styles.rootContainer}>
-                <p className={{ root: classes.paragraphy }}>Playlist is empty now, please add songs</p>
-                <TextField
-                    classes={{ root: classes.textField }}
-                    id="standard-basic"
-                    variant="outlined"
-                    size="large"
-                    label="Search songs code"
-                    InputProps={{
-                        style: { color: '#fff' },
-                    }}
-                    InputLabelProps={{
-                        style: { color: '#fff' },
-                    }}
-                />
-            </div>
-            <div>
-                <p className={{ root: classes.headLine }}>Device</p>
-                <Button className={{ root: classes.primaryButton }}
-                    onClick={handleFetch}>
-                </Button>
-            </div>
-
-            <div>
-                <p className={{ root: classes.headLine }}>Active Friends</p>
+            <div className={styles.bodyPartContainer}> 
+                <form>
+                    <p className={styles.paraStyle}>Playlist is empty now, please add songs</p>
+                    <TextField
+                        classes={{ root: classes.textField }}
+                        id="standard-basic"
+                        variant="outlined"
+                        size="large"
+                        label="Search songs code"
+                        InputProps={{
+                            style: { color: '#fff' },
+                        }}
+                        InputLabelProps={{
+                            style: { color: '#fff' },
+                        }}
+                    />
+                    <p className={styles.paraStyle}>Device</p>
+                    <Button classes={{ root: classes.deviceButton }}
+                        onClick={handleFetch}>Fetch Device
+                    </Button>
+                </form>
             </div>
 
         </div>
