e10f9563659fa6db4daa1dda9b01957d984c2318
jbur801 <jbur801@aucklanduni.ac.nz>
2021-04-25 12:39:50 +1200

    redid backend, now uses proxy

--- backend/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-import login from './login';
-router.use('login', login);
-
-export default router;
--- backend/src/routes/login/login.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.post('/', (req, res) => {
-    //idk chief
-    //TODO Spotify API login and storing token on DB
-});
