f846d47b0b11503728de9bdf84f34fad3b5c3def
闵静纬 <minjingwei@minjingweideMacBook-Pro.local>
2021-05-02 13:16:40 +0800

    update table in homePage

--- frontend/src/homePage/index.js
+++ frontend/src/homePage/index.js
@@ -3,6 +3,7 @@ import styles from "./style.module.css";
 import { Button, TextField, makeStyles, MenuItem, Menu, Table, TableBody, TableCell, TableHead, TableRow, } from '@material-ui/core';
 import Popper from '@material-ui/core/Popper';
 import { TableContainer } from '@material-ui/core';
+import Paper from '@material-ui/core/Paper';
 
 
 const useStyles = makeStyles({
@@ -18,6 +19,7 @@ const useStyles = makeStyles({
         width: "300px",
         fontSize: 18,
         color: "white",
+        margin: "10px"
     },
     menuButton: {
         background: "#30A0F5",
@@ -28,7 +30,16 @@ const useStyles = makeStyles({
         
     },
     table: {
-        minWidth: "300px",
+        width: 300,
+        background: "#595959",
+        margin: "10px",
+    },
+    showPlayListButton: {
+        background: "#30A0F5",
+        borderRadius: 30,
+        width: "60%",
+        padding: "18px 40px",
+        fontSize: 18,
     },
 });
 
@@ -37,6 +48,10 @@ export default function HomePage() {
     const classes = useStyles();
     
     const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const [anchorEl2, setAnchorEl2] = React.useState(null);
+
+    const [playList, setPlayList] = React.useState(null);
 
     const [tableAnchorEl, setTableAnchorEl] = React.useState(null);
 
@@ -55,11 +70,35 @@ export default function HomePage() {
 
         const handleOpenTable = (event) => {
             setTableAnchorEl(tableAnchorEl ? null : event.currentTarget);
+            setPlayList(true)
         };
 
+        
         const openTable = Boolean(tableAnchorEl);
-    
 
+        const handleOpenSongsMenu = (event) => {
+            setAnchorEl2(event.currentTarget);
+        };
+
+        const handleSongsClose = () => {
+            setAnchorEl2(null);
+        };
+        
+        const addSong = (playList) => {
+            setPlayList((prev) => {
+              return [playList, ...prev];
+            });
+          };
+
+        /*
+        const handlePlayListTabel = () => {
+            if(table){
+                return <p className={styles.paraStyle}>Playlist is empty now, please add songs</p>
+            }else{
+                return <Table></Table>
+            }
+        }
+        */
     
 
 
@@ -69,15 +108,15 @@ export default function HomePage() {
                 <Button classes={{ root: classes.backButton }}
                     onClick={handleBack}>Back
                 </Button>
-                    <Button className={styles.menuButton} 
+                <Button className={styles.menuButton} 
                     classes={{ root: classes.menuButton }} 
                     onClick={handleOpenMenu}
                     aria-controls="menu-list"
                     aria-haspopup="true"
                     >
                     Open Menu
-                    </Button>
-                    <Menu
+                </Button>
+                <Menu
                     id="menu-list"
                     anchorEl={anchorEl}
                     keepMounted
@@ -87,15 +126,15 @@ export default function HomePage() {
                     <MenuItem onClick={handleClose}>Login</MenuItem>
                     <MenuItem onClick={handleClose}>My account</MenuItem>
                     <MenuItem onClick={handleClose}>Logout</MenuItem>
-                    </Menu>
+                </Menu>
             </div>
 
             <div className={styles.bodyPartContainer}> 
                 <form>
-                <p className={styles.paraStyle}>Playlist is empty now, please add songs</p>
+                <p className={styles.paraStyle}>Playlist {playList ? 'is below' : 'is empty, please add songs' }</p>
                 <TextField
-                        onClick={handleOpenTable}
-                        aria-controls="table-list"
+                        onClick={handleOpenSongsMenu}
+                        aria-controls="songs-list"
                         aria-haspopup="false"
                         classes={{ root: classes.textField }}
                         id="standard-basic"
@@ -109,24 +148,44 @@ export default function HomePage() {
                             style: { color: '#fff' },
                         }}
                     />
-                    <Popper id="table-list" open={openTable} anchorEl={tableAnchorEl}>
-                        <Table className={{root: classes.table}}>
-                            <TableHead>
-                            <TableRow>
+                <Menu
+                    id="songs-list"
+                    anchorEl={anchorEl2}
+                    keepMounted
+                    open={Boolean(anchorEl2)}
+                    onClose={handleSongsClose}
+                    >
+                    <MenuItem onClick={() => setPlayList('Song 1') }>Song 1</MenuItem>
+                    <MenuItem onClick={() => setPlayList('Song 2')}>Song 2</MenuItem>
+                    <MenuItem onClick={() => setPlayList('Song 3')}>Song 3</MenuItem>
+                </Menu>
+
+
+                <Button classes={{ root: classes.showPlayListButton }} 
+                    onClick={handleOpenTable}
+                    aria-describedby="playlist-table"
+                    >Show playlist
+                </Button>
+                <Popper id="playlist-table" open={openTable} anchorEl={tableAnchorEl}>
+                    <Table classes={{ root: classes.table }}>
+                        <TableHead>
+                            <TableRow style={{backgroundColor:'gray'}}>
                                 <TableCell align="centre">Songs code</TableCell>
                                 <TableCell align="centre">Songs name</TableCell>
                                 
                             </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                <TableRow>
-                                <TableCell align="centre">code here</TableCell>
-                                <TableCell align="centre">name here</TableCell>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow style={{backgroundColor:'gray'}}>
+                                <TableCell align="centre">Code</TableCell>
+                                <TableCell align="centre">
+                                    {playList}
+                                </TableCell>
                             
-                                </TableRow>
-                            </TableBody>
-                        </Table>
-                    </Popper>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </Popper>
                 </form>
             </div>
 
