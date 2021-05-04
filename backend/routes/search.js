691621d7e555db10ddabfb3ce8a7b7592ddee3ac
jbur801 <jbur801@aucklanduni.ac.nz>
2021-05-05 00:05:32 +1200

    removed an unnecassary file

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -11,7 +11,7 @@ router.get('/search', async (req, res)  => {
     const headers = {
         "Accept": "application/json",
         "Content-Type": "application/json",
-        "Authorization": "Bearer BQBRrGS4sWwq2409hALZi0hyPi7erzP7TqqKuFjKEyA4yed_7tFQVzjpseKVAL3oUOuCDQelbCE1Odoom_0xGHS7NMBStXcNFXRSEhpcBrrRu5DOnjresKxdiamYRyth6GPnJ810GKVl",
+        "Authorization": "Bearer BQCTQ-jWwXNzphLw3jbFse5FBQfI6TwUQbL_WMd_u1-3nhzJuXP818zReUVIKgz-OkJ5mIM5RaZX3kLGrM__r_b1e_W8GnF3tVfWjeQydqzlPlqoODo8Vt2McOM2vM87r3j7x7ui0j6wD74yIi_AyakR6w",
     }
     console.log(req.query.searchTerm);
     var response = await axios.get(spotify.createSearchQuery(req.query.searchTerm), {
