a1179f355592b872a9e99d870e082ce19c048048
jshi213 <jshi213@aucklanduni.ac.nz>
2021-05-05 23:31:38 +1200

    added async awaits in testdb and queries

--- backend/db/test.js
+++ backend/db/test.js
@@ -1,41 +1,50 @@
 const query = require("./queries");
 const connect = require("./connect");
 
-var testSong = {
-  name: "the wheels on the bus",
-  imageURL: "irefuse",
-  artistNames: "no",
-  songDuration: 10,
-};
+main();
 
-var testToken = {
-  accessToken : makeCode(6),
-  refreshToken : makeCode(6)
-}
-var testUser = {
-  userName: makeCode(5),
-};
-const TESTCONNECTIONSTRING =
-  "mongodb+srv://smaltadmin:AG47gX6WIP3YUaB2@cluster0.knzot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+async function main() {
 
-if (connect.connectToDatabase(TESTCONNECTIONSTRING)) {
-  console.log("connection success");
-}
-//query.clearDB();
-var roomCode = query.createNewRoom(testToken, makeCode(4));
+  var testSong = {
+    name: "the wheels on the bus",
+    imageURL: "irefuse",
+    artistNames: "no",
+    songDuration: 10,
+    id: makeCode(10),
+  };
 
-//query.addSongToPool(testSong, roomCode);
-//query.addUserToRoom(testUser, roomCode);
+  var testUser = {
+    userName: makeCode(5),
+  };
 
-function makeCode(length) {
-  var result = [];
-  var characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    result.push(
-      characters.charAt(Math.floor(Math.random() * charactersLength))
-    );
+  const testToken = {
+    accessToken: makeCode(5),
+    refreshToken: makeCode(5),
   }
-  return result.join("");
+
+  const TESTCONNECTIONSTRING =
+    "mongodb+srv://smaltadmin:AG47gX6WIP3YUaB2@cluster0.knzot.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+  if (connect.connectToDatabase(TESTCONNECTIONSTRING)) {
+    console.log("connection success");
+  }
+  await query.clearDB();
+  var roomCode = await query.createNewRoom(testToken, makeCode(4));
+
+  await query.addSongToPool(testSong, roomCode);
+  await query.addUserToRoom(testUser, roomCode);
+  await query.getCompleteRoom(roomCode);
+
+  function makeCode(length) {
+    var result = [];
+    var characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+      result.push(
+        characters.charAt(Math.floor(Math.random() * charactersLength))
+      );
+    }
+    return result.join("");
+  }
 }
