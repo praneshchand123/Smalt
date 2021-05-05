25e7cdcec6ed6af53bc8688b14beddd358332c59
闵静纬 <minjingwei@minjingweideMacBook-Pro.local>
2021-05-05 11:48:37 +0800

    remove useState input

--- frontend/src/components/CodeInput/index.js
+++ frontend/src/components/CodeInput/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./style.module.css";
 import { useHistory } from "react-router-dom";
 import { Button, TextField, makeStyles } from "@material-ui/core";
