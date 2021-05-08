b0b7a447d8980f5ee87a282b55f24ad6691524a8
jshi213 <jshi213@aucklanduni.ac.nz>
2021-05-09 04:52:53 +1200

    like button for songs now increment/decrement upvotecount for songs in DB

--- frontend/src/components/Song/index.js
+++ frontend/src/components/Song/index.js
@@ -1,52 +1,63 @@
 import React, { useState } from "react";
 import styles from "./style.module.css";
-import { useHistory } from "react-router-dom";
-import { Button, TextField, makeStyles } from "@material-ui/core";
 import axios from "axios";
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import { useCookies } from "react-cookie";
 import IconButton from "@material-ui/core/IconButton";
-import FavoriteIcon from '@material-ui/icons/Favorite'
-
-import {
-    Avatar,
-    TableCell,
-    TableRow,
-  } from "@material-ui/core";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import { Avatar, TableCell, TableRow } from "@material-ui/core";
 
 export default function Song(props) {
-    console.log("I was called");
-    const song = props.props;
-    const [likeButtonColor, setLikeButtonColor] = useState('inherit');
-    const handleLikeButton = () => {
-        if(likeButtonColor !== 'inherit'){
-            setLikeButtonColor('inherit');
-        }else{
-            setLikeButtonColor('secondary');
-        }
+  const song = props.props;
 
+  const [likeButtonColor, setLikeButtonColor] = useState("inherit");
+  const [liked, setLikedStatus] = useState(false);
+  const [cookies, setCookie] = useCookies(["name"]);
+
+  const handleLikeButton = async () => {
+    setLikedStatus(!liked);
+    var voteType;
+    if (likeButtonColor !== "inherit") {
+      setLikeButtonColor("inherit");
+    } else {
+      setLikeButtonColor("secondary");
+    }
+    if (liked) {
+        voteType = "unvote";
+    } else {
+        voteType = "upvote";
+    }
+    const toUpdate = {
+      room: cookies.room.id,
+      track: song,
+      voteType: voteType,
     };
-    return (
-        <>
-            <TableRow className={styles.tableRow}>
-            <TableCell className={styles.tableCell}>
-                    <Avatar
-                        variant="rounded"
-                        src={song.imageURL}
-                        className={styles.image}
-                    />
-                </TableCell>
-                <TableCell className={styles.tableCellHead}>
-                    <div className ={styles.songName} >{song.name}</div>
-                    <div className ={styles.songArtist}>{song.artists}</div>
-                </TableCell>
-                <TableCell className={styles.tableCell}>
-                    <IconButton onClick={handleLikeButton} color={likeButtonColor}>
-                        <FavoriteIcon />
-                    </IconButton>
-                </TableCell>
-            </TableRow>
-        </>
+    console.log(toUpdate);
+    const response = await axios.put(
+      "http://localhost:3001/users/song/vote",
+      toUpdate
     );
+  };
+
+  return (
+    <>
+      <TableRow className={styles.tableRow}>
+        <TableCell className={styles.tableCell}>
+          <Avatar
+            variant="rounded"
+            src={song.imageURL}
+            className={styles.image}
+          />
+        </TableCell>
+        <TableCell className={styles.tableCellHead}>
+          <div className={styles.songName}>{song.name}</div>
+          <div className={styles.songArtist}>{song.artists}</div>
+        </TableCell>
+        <TableCell className={styles.tableCell}>
+          <IconButton onClick={handleLikeButton} color={likeButtonColor}>
+            <FavoriteIcon />
+          </IconButton>
+        </TableCell>
+      </TableRow>
+    </>
+  );
 }
-
-
