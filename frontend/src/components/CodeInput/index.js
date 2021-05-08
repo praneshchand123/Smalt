745645f18357a97a9052f9254e6f4f731c58aa23
jbur801 <jbur801@aucklanduni.ac.nz>
2021-05-09 03:13:46 +1200

    I think the join fix wasn't added to the previous commit

--- frontend/src/components/CodeInput/index.js
+++ frontend/src/components/CodeInput/index.js
@@ -5,7 +5,7 @@ import { Button, TextField, makeStyles } from "@material-ui/core";
 import { useCookies } from 'react-cookie';
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import IconButton from "@material-ui/core/IconButton";
-
+import axios from "axios";
 export default function CodeInput() {
   const history = useHistory();
 
@@ -13,52 +13,65 @@ export default function CodeInput() {
   const [cookies, setCookie] = useCookies(['room']);
   const [textValue, setTextValue] = React.useState("");
 
-  const handleEnter = () => {
+  const handleEnter = async () => {
+    console.log(`handle ${textValue}`)
     if (textValue !== "") {
-      var cookie ={id :textValue,
-        isHost :false}
-      setCookie('room', cookie, { path: '/' })
-      history.push("/home");
-    } else {
-      setEmptyFieldFlag(true);
+      console.log("not empty")
+      var params = {
+        roomCode: textValue
+      }
+      axios.get("http://localhost:3001/join", { params: params }).then((res) => {
+        console.log("valid room entered")
+        var cookie = {
+          id: textValue,
+          isHost: false
+        }
+        setCookie('room', cookie, { path: '/' })
+        history.push("/home");
+      }).catch(function (error) {
+        console.log(error);
+        setEmptyFieldFlag(true);
+      });
+    } else{
+      console.log("empty")
     }
   };
 
-  const handleBack = () => {
-    history.goBack();
-  };
+    const handleBack = () => {
+      history.goBack();
+    };
 
-  return (
-    <>
-      <IconButton className={styles.backButton} onClick={handleBack}>
-        <ArrowBackIosIcon />
-      </IconButton>
-      <div className={styles.textFieldContainer}>
-        <TextField
-          noValidate
-          autoComplete="off"
-          className={styles.textField}
-          id="standard-basic"
-          variant="outlined"
-          label="Playlist code"
-          value={textValue}
-          error={emptyFieldFlag}
-          helperText={emptyFieldFlag && "Please enter a username"}
-          onChange={(e) => {
-            setTextValue(e.target.value);
-            setEmptyFieldFlag(false);
-          }}
-          InputProps={{
-            style: { color: "#fff" },
-          }}
-          InputLabelProps={{
-            style: { color: "#fff" },
-          }}
-        />
-        <Button className={styles.enterButton} onClick={handleEnter}>
-          Enter
+    return (
+      <>
+        <IconButton className={styles.backButton} onClick={handleBack}>
+          <ArrowBackIosIcon />
+        </IconButton>
+        <div className={styles.textFieldContainer}>
+          <TextField
+            noValidate
+            autoComplete="off"
+            className={styles.textField}
+            id="standard-basic"
+            variant="outlined"
+            label="Playlist code"
+            value={textValue}
+            error={emptyFieldFlag}
+            helperText={emptyFieldFlag && "invalid room code"}
+            onChange={(e) => {
+              setTextValue(e.target.value);
+              setEmptyFieldFlag(false);
+            }}
+            InputProps={{
+              style: { color: "#fff" },
+            }}
+            InputLabelProps={{
+              style: { color: "#fff" },
+            }}
+          />
+          <Button className={styles.enterButton} onClick={handleEnter}>
+            Enter
         </Button>
-      </div>
-    </>
-  );
-}
+        </div>
+      </>
+    );
+  }
